7fe8137b9c23ed85ee335900d4e8791aafa454a3
Dineo Moseki <yesdineo@gmail.com>
2023-07-18 10:08:35 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - A program that prints _putchar, followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+printf("_putchar\n");
+
+return (0);
+}
+
