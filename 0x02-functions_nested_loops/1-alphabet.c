d2b813faf7972cba5d0716ff78a4959bc935fafe
Dineo Moseki <yesdineo@gmail.com>
2023-07-18 13:41:56 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - A function that prints the alphabet, in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char al;
+
+	for (al = 'a'; al <= 'z'; al++)
+	       _putchar(al);
+
+_putchar('\n');
+}
