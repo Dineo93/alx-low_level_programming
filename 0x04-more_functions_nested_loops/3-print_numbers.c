6338416cca0aa486148adbbbd172ad502962e5ed
Dineo Moseki <yesdineo@gmail.com>
2023-07-20 11:08:56 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
  * print_numbers - A function that prints the numbers, from 0 to 9
  *
- * Return: no return
+ * Return: 0 up to 9
  */
+
 void print_numbers(void)
 {
 int a;
-for (a = 48; a <= 57; a++)
+for (a = 0; a <= 9; a++)
 {
-_putchar(a);
+_putchar(a + '0');
 }
 _putchar('\n');
 }
