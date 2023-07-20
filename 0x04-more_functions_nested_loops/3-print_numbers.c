a2c30fe5f35223056a89510df6f65b0aba7b40da
Dineo Moseki <yesdineo@gmail.com>
2023-07-20 05:57:40 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - A function that prints the numbers, from 0 to 9
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+int c;
+for (c = 'A'; c < 'Z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
