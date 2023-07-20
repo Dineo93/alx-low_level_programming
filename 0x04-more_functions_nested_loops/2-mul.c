c8ed0e1a0e14a916bab5006f78a8140b0164123a
Dineo Moseki <yesdineo@gmail.com>
2023-07-20 05:45:18 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - A  function that multiplies two integers
+ * @a: a - character
+ * @b: b - character
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+int mul;
+mul = a * b;
+return (mul);
+}
