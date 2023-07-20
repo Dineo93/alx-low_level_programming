43b5a965d9d54a0ce8916395c144c7140a8c779c
Dineo Moseki <yesdineo@gmail.com>
2023-07-20 05:11:17 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - A function that checks for a digit (0 through 9)
+ * @c: character to be checked
+ * Return: 1 if c is a digit, 0 if otherwise
+ */
+int _isdigit(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+return (0);
+}
