2564691252e1c4f01791c987f7b53b9563cada55
Dineo Moseki <yesdineo@gmail.com>
2023-07-24 14:43:51 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,17 @@
 #include "main.h"
+
 /**
  * _puts - prints a string
  * @str: print string
+ *Return: new line and string
  */
 void _puts(char *str)
 {
-while (*str != '\0')
+int i = 0;
+while (str[i])
 {
-_putchar(*str++);
+_putchar(str[i]);
+i++;
 }
-_putchar('\0');
+_putchar('\n');
 }
