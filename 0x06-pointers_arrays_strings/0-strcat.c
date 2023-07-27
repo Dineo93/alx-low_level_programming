e77ad1f22d323dbee548bee4cca47ac541145490
Dineo Moseki <yesdineo@gmail.com>
2023-07-27 10:52:15 -0700

    0-main.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ * Return: void
+ */
+char *_strcat(char *dest, char *src);
+{
+int i;
+int a;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+a = 0;
+while (src[j] != '\0')
+{
+dest[i] = src[a];
+i++;
+a++;
+}
+dest[i] = '\0';
+return (dest);
+}
